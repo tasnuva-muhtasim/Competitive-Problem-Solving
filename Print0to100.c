710204eb89e6de643402aa1adc10813e1f3470dc
tasnuva-muhtasim <88622706+tasnuva-muhtasim@users.noreply.github.com>
2021-09-12 01:47:05 +0600

    Create Print0to100.c

---
+++ Print0to100.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+
+{
+int i,a;
+printf("Enter a number to start the program\n");
+scanf("%d",&a);
+for(i=0;i<=100;i++)
+{
+  printf("%d\t",i);
+}
+return 0;
+}
