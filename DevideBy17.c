54bcecdab76ad7af4bc94795281a90a7e4abddb4
tasnuva-muhtasim <88622706+tasnuva-muhtasim@users.noreply.github.com>
2021-09-12 01:34:21 +0600

    Create DevideBy17.c

---
+++ DevideBy17.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+
+{
+int i,a;
+printf("Enter a number to start the program\n");
+scanf("%d",&a);
+for(i=17;i<101;i++)
+{
+  i%17==0;
+  printf("%d\t",i);
+}
+return 0;
+}
