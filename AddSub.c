de540fa83d44c6dcfdcbcd63d99f12c7fab62065
tasnuva-muhtasim <88622706+tasnuva-muhtasim@users.noreply.github.com>
2021-09-12 00:57:31 +0600

    Create AddSub.c

---
+++ AddSub.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c ;
+    printf("Input Two Number\n");
+    scanf("%d",&a );
+    scanf("%d" ,&b );
+    printf("For addition input 1 and for substraction input 0\n");
+    scanf("%d",&c);
+    if(c==1)
+    {
+        int sum;
+        sum = a + b;
+        printf("sum = %d",sum);
+    }
+    else
+    {
+        int Sub;
+      Sub = a-b;
+        printf("Sub = %d",Sub);
+    }
+    return 0;
+}
+
+
