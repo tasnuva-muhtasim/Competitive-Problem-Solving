202d1422fcf8fddd0819940c561623815f871e8c
tasnuva-muhtasim <88622706+tasnuva-muhtasim@users.noreply.github.com>
2021-09-19 15:23:32 +0600

    Create VowelFinder.c

---
+++ VowelFinder.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    char chr,a,e,i,o,u;
+    printf("Input a character to find-out if it's vowel\n");
+    scanf("%c",&chr);
+    if(chr == 'a')
+    {
+        printf("It is a vowel.\n");
+    }
+    else if(chr =='e')
+    {
+        printf("It is a vowel.\n");
+    } 
+    else if(chr =='i')
+    {
+        printf("It is a vowel.\n");
+    } 
+    else if(chr =='o')
+    {
+        printf("It is a vowel.\n");
+    } 
+    else if(chr =='u')
+    {
+        printf("It is a vowel.\n");
+    } 
+    else
+    {
+        printf("It is a consonant");
+    }
+    return 0;
+}
